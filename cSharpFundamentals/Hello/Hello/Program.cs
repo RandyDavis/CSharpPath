a4a872aefba8f7bb746c749ffcc99759fd9bb922
Randy Davis <randy_davis@outlook.com>
2016-06-28 11:45:01 -0500

    complete "Are you Sleepy?" lesson
    write a quick program that makes prompts and determines if you need more
    sleep

--- cSharpFundamentals/Hello/Hello/Program.cs
+++ cSharpFundamentals/Hello/Hello/Program.cs
@@ -7,7 +7,22 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, " + args[0]);
+            Console.WriteLine("Your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more sleep");
+            }
         }
     }
 }
