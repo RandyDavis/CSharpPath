64fb64026a5754a1af02072efc5700f6058d7289
Randy Davis <randy_davis@outlook.com>
2016-07-06 14:19:53 -0500

    complete Storing Derived  Instances in Arrays
    add ArrayTyping project with example of derived types

---
+++ cSharpCollectionsFundamentals/04-TheArrayType/ArrayTyping/ArrayTyping/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTyping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] objArray = new object[3]
+            {
+                "Hello World",
+                4,
+                new Button {Text = "CLick me!" }
+            };
+
+            foreach (object item in objArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    internal class Button
+    {
+        public string Text { get; set; }
+    }
+}
