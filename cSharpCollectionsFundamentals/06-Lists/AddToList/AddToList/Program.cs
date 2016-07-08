8c03da083efd7ac25701afc3a961c157ff7c88d1
Randy Davis <randy_davis@outlook.com>
2016-07-08 15:05:07 -0500

    create 06-Lists directory; complete Introducing List<T> lesson
    add AddToList project showing how to add values to a list vs an array

---
+++ cSharpCollectionsFundamentals/06-Lists/AddToList/AddToList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presidents = new List<string>
+            {
+                "Jimmy Carter",
+                "Ronald Reagan",
+                "George HW Bush",
+                "Bill Clinton",
+                "George W Bush"
+            };
+            presidents.Add("Barack Obama");
+
+            foreach (string president in presidents)
+            {
+                Console.WriteLine(president);
+            }
+        }
+    }
+}
