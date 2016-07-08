04c6c829e9c333097b2d64b06215313790465548
Randy Davis <randy_davis@outlook.com>
2016-07-08 15:18:01 -0500

    complete List<T> Under The Hood
    add demos to show how List<T> works behind the scenes

--- cSharpCollectionsFundamentals/06-Lists/AddToList/AddToList/Program.cs
+++ cSharpCollectionsFundamentals/06-Lists/AddToList/AddToList/Program.cs
@@ -10,7 +10,7 @@ namespace AddToList
     {
         static void Main(string[] args)
         {
-            var presidents = new List<string>
+            var presidents = new List<string>(12)
             {
                 "Jimmy Carter",
                 "Ronald Reagan",
@@ -18,8 +18,23 @@ namespace AddToList
                 "Bill Clinton",
                 "George W Bush"
             };
+            Console.WriteLine("Before: ");
+            Console.WriteLine("Count= " + presidents.Count);
+            Console.WriteLine("Capacity = " + presidents.Capacity + "\r\n");
             presidents.Add("Barack Obama");
+            presidents.Add("Bill Gates");
+            presidents.Add("Joss Whedon");
+            presidents.Add("John Papa");
 
+            /* REMOVE an item
+            presidents.Remove("Bill Gates");  // Expensive if item to be removed is at beginning of list
+            presidents.RemoveAt(5);  // Always better to use this one
+            */
+            Console.WriteLine("\r\nAfter adding: ");
+            Console.WriteLine("Count= " + presidents.Count);
+            Console.WriteLine("Capacity = " + presidents.Capacity + "\r\n");
+
+            // string who = presidents[10];  // Shows out of range exception
             foreach (string president in presidents)
             {
                 Console.WriteLine(president);
