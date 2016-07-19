4fc80ef06f20398011422c2a61df2e4265ad5121
Randy Davis <randy_davis@outlook.com>
2016-07-19 14:36:38 -0500

    add 08-Dictionaries section directory; complete Introducing Dictionary<TKey, TValue>
    add PrimeMinisters project that will serve as base code for enter
    section

---
+++ cSharpCollectionsFundamentals/08-Dictionaries/TKeyTValue/PrimeMinisters/PrimeMinisters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMinisters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new Dictionary<string, PrimeMinister>
+            {
+                {"JC", new PrimeMinister("James Callaghan", 1976) },
+                {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                {"TB", new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            foreach (var pm in primeMinisters)
+            {
+                Console.WriteLine(pm);
+            }
+        }
+    }
+}
