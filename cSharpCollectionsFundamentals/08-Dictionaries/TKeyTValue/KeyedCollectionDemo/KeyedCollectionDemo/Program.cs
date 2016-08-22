0426bb353c46f72fdfc673e270cca4e5947c700b
Randy Davis <randy_davis@outlook.com>
2016-08-22 14:28:18 -0500

    Introducing KeyedCollection<TKey, TValue>
    complete Introducing KeyedCollection<TKey, TValue>

---
+++ cSharpCollectionsFundamentals/08-Dictionaries/TKeyTValue/KeyedCollectionDemo/KeyedCollectionDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrimeMinisters;
+using System.Collections.ObjectModel;
+
+namespace KeyedCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new PrimeMinistersByYearDictionary()
+            {
+                new PrimeMinister("James Callaghan", 1976),
+                new PrimeMinister("Margaret Thatcher", 1979),
+                new PrimeMinister("Tony Blair", 1997)
+            };
+            primeMinisters.Add(new PrimeMinister("John Major", 1990));
+
+            Console.WriteLine("Tony is " + primeMinisters[1997]);
+
+            foreach (var pm in primeMinisters)
+            {
+                Console.WriteLine(pm);
+            }
+        }
+    }
+
+    class PrimeMinistersByYearDictionary : KeyedCollection<int, PrimeMinister>
+    {
+        protected override int GetKeyForItem(PrimeMinister item)
+        {
+            return item.YearElected;
+        }
+    }
+}
