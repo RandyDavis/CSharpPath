c54e83c639a414090dd5687bd43bb37095ae486e
Randy Davis <randy_davis@outlook.com>
2016-08-22 15:25:18 -0500

    Set Comparisons and Subsets
    complete Set Comparisons and Subsets

---
+++ cSharpCollectionsFundamentals/09-Sets/SetComparisons/SetComparisons/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetComparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ukCities = new HashSet<string>
+            {
+                "Sheffield", "Ripon", "Truro", "Manchester"
+            };
+
+            var bigUKCities = new HashSet<string>
+            {
+                "Sheffield", "Manchester"
+            };
+
+            var bigCitiesArr = new HashSet<string>
+            {
+                "New York", "Manchester", "Sheffield", "Paris"
+            };
+
+            //bool ukIsSubset = ukCities.IsSubsetOf(bigCitiesArr);
+            bool ukIsSubset = bigCitiesArr.IsSupersetOf(ukCities);
+            Console.WriteLine("UK cities subset of big cities? " + ukIsSubset);
+
+            //bool bigUKIsSubset = bigUKCities.IsSubsetOf(bigCitiesArr);
+            bool bigUKIsSubset = bigCitiesArr.IsSupersetOf(bigUKCities);
+            Console.WriteLine("Big UK cities subset of big cities? " + bigUKIsSubset);
+        }
+    }
+}
