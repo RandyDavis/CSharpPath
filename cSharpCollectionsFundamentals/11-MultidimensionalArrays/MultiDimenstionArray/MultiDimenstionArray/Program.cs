fd23b4fa76ee01a6167f470591a5e90c14434a93
Randy Davis <randy_davis@outlook.com>
2016-08-23 15:37:39 -0500

    Length and Rank
    complete Length and Rank

--- cSharpCollectionsFundamentals/11-MultidimensionalArrays/MultiDimenstionArray/MultiDimenstionArray/Program.cs
+++ cSharpCollectionsFundamentals/11-MultidimensionalArrays/MultiDimenstionArray/MultiDimenstionArray/Program.cs
@@ -11,18 +11,20 @@ namespace MultiDimenstionArray
         static void Main(string[] args)
         {
             float[,] tempsGrid = new float[4, 3];
+            Console.WriteLine("The length is " + tempsGrid.Length);
+            Console.WriteLine("The rank is " + tempsGrid.Rank);
 
-            for (int x = 0; x < 4; x++)
+            for (int x = 0; x < tempsGrid.GetLength(0); x++)
             {
-                for (int y = 0; y < 3; y++)
+                for (int y = 0; y < tempsGrid.GetLength(1); y++)
                 {
                     tempsGrid[x, y] = x + 10 * y;
                 }
             }
            
-            for (int x = 0; x < 4; x++)
+            for (int x = 0; x < tempsGrid.GetLength(0); x++)
             {
-                for (int y = 0; y < 3; y++)
+                for (int y = 0; y < tempsGrid.GetLength(1); y++)
                 {
                     Console.Write(tempsGrid[x, y] + ", ");
                 }
