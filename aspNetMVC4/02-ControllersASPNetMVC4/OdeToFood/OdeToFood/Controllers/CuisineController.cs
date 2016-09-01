3bf236862edd77172ee6cfec670866c5883287cc
Randy Davis <randy_davis@outlook.com>
2016-09-01 13:41:08 -0500

    complete Action Results
    complete Action Results - shows how to return different types of actions
    within controllers

--- aspNetMVC4/02-ControllersASPNetMVC4/OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ aspNetMVC4/02-ControllersASPNetMVC4/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -14,8 +14,11 @@ namespace OdeToFood.Controllers
         public ActionResult Search(string name = "French")
         {
             var message = Server.HtmlEncode(name);
-
-            return Content(message);
+            // Example redirects
+            //return RedirectToAction("Index", "Home", new { name = name });
+            //return RedirectToRoute("Default", new { controller = "Home", action = "About" });
+            //return File(Server.MapPath("~/Content/site.css"), "text/css");
+            return Json(new { Message = message, Name = "Jim" }, JsonRequestBehavior.AllowGet);
         }
 
     }
