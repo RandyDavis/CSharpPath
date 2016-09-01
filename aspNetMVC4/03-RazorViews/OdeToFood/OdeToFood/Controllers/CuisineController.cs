5c0935123b2614ef6f6023c3460e905b70e7dad4
Randy Davis <randy_davis@outlook.com>
2016-09-01 14:51:10 -0500

    create 03-RazorViews
    create 03-RazorViews; complete Razor Basics - createed Reviews directory
    in Views directory that will hold revies template along with
    RestaurantReview model and updats to ReviewsController

---
+++ aspNetMVC4/03-RazorViews/OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    //[Authorize]  // Can put Authorize here to make it controller level
+    [LogAttribute]
+    public class CuisineController : Controller
+    {
+
+        //[Authorize] // Can put Authorize here to make it method/ActionResult level
+        public ActionResult Search(string name = "French")
+        {
+            throw new Exception("Something bad happened!");
+            var message = Server.HtmlEncode(name);
+            return Content(message);
+        }
+
+
+    }
+}
